ab0c1d61ed4ca6cb625c98f364898bad184349e2
Abdo <abdopoetry@gmail.com>
2019-11-28 23:35:37 +0200

    Add .prettierrc

--- cogni-world-backend/config/dbConnect.js
+++ cogni-world-backend/config/dbConnect.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const { mongoURI } = require('../keys.secret');
 
-const mongoConfig = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+const mongoConfig = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
 
 mongoose
   .connect(mongoURI, mongoConfig)
