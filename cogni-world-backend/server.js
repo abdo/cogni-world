de1f62a1522dd8ed01536a044420e04558d84346
Abdo <abdopoetry@gmail.com>
2019-11-28 22:43:28 +0200

    Build backend skeleton

---
+++ cogni-world-backend/server.js
@@ -0,0 +1,28 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const passport = require('passport');
+
+const app = express();
+
+// Models
+
+// BodyParser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Connect
+
+// DB Config
+
+// Passport Config
+
+// Cors Middleware
+app.use(cors());
+
+// Routes
+app.use('/api/info', (req, res) => res.send('Cogni World App API'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
