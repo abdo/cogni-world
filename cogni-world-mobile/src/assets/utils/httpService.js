4dd4e91e9c878096a243b2c92cec834cc8cbd5e2
Abdo <abdopoetry@gmail.com>
2019-11-30 15:30:06 +0200

    Create httpService

---
+++ cogni-world-mobile/src/assets/utils/httpService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+import keys from '../../keys.secret';
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  patch: axios.patch,
+  delete: axios.delete,
+};
+
+export default http;
+
+export const setAuthToken = token => {
+  if (token) {
+    // Apply to every request
+    axios.defaults.headers.common.Authorization = token;
+  }
+};
+
+export const removeAuthToken = () => {
+  // Delete auth header
+  delete axios.defaults.headers.common.Authorization;
+};
+
+export const { serverPath } = keys;
+export const userAPI = `${serverPath}/api/user`;
