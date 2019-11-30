3fd910fb94268e7056cabee0f92b818100e6886a
Abdo <abdopoetry@gmail.com>
2019-12-01 01:19:08 +0200

    Disallow regular user to change user verified status

--- cogni-world-backend/controllers/userController/updateUser.js
+++ cogni-world-backend/controllers/userController/updateUser.js
@@ -8,13 +8,17 @@ module.exports = (req, res) => {
 
   // Operation is allowed if: doer is admin or doer is same user being updated
   const isSameUser = req.user._id.toString() === params.userId.toString();
+  const isAdmin = req.user.isAdmin;
 
-  if (!isSameUser && !req.user.isAdmin) {
+  const newUserinfo = { ...req.body };
+
+  if (
+    (!isAdmin && !isSameUser) ||
+    (!isAdmin && Object.keys(newUserinfo).includes('verified'))
+  ) {
     const message = 'You are not allowed to perform this operation';
     return res.status(401).json({ message });
   }
-
-  const newUserinfo = { ...req.body };
 
   // Resetting values back to their default value if they come as ''
   Object.keys(User.schema.paths).forEach(key => {
